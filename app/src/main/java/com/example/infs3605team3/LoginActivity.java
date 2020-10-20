b612fe926dc9665362fcf2e8e737c310c007e2d2
XueBai0125 <bx1020689117@gmail.com>
2020-10-20 17:17:58 +0800

    E-mail verification

--- app/src/main/java/com/example/infs3605team3/LoginActivity.java
+++ app/src/main/java/com/example/infs3605team3/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.infs3605team3;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,15 +14,21 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.gyf.immersionbar.ImmersionBar;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText etEmail;
     private EditText etPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,11 +38,54 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initView();
+        mAuth = FirebaseAuth.getInstance();
         ImmersionBar.with(this)
                 .transparentStatusBar()
                 .statusBarDarkFont(false)
                 .init();
 
+    }
+
+    private void updateUserStatus() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user!=null){
+            if (!user.isEmailVerified()) {
+                new AlertDialog.Builder(this)
+                        .setTitle("提示")
+                        .setMessage("邮箱尚未验证,请发送邮件认证")
+                        .setPositiveButton("发送邮件", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                user.sendEmailVerification()
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Toast.makeText(LoginActivity.this, "己成功送驗證郵件，請檢查你的電子郵件信箱/Please check your email", Toast.LENGTH_LONG).show();
+
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this, "無法送出驗證郵件/Can't send verification email：" + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+
+                                    }
+                                });
+                            }
+                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create().show();
+
+            }else{
+                Intent intent = new Intent();
+                intent.setClass(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
     }
 
     private void initView() {
@@ -70,18 +120,15 @@ public class LoginActivity extends AppCompatActivity {
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Intent intent = new Intent();
-                            intent.setClass(LoginActivity.this, MainActivity.class);
-                            startActivity(intent);
-                            finish();
-                        } else {
-                            Toast.makeText(LoginActivity.this,"failed, Check Email or Password",Toast.LENGTH_SHORT).show();
-                        }
 
-
+                        updateUserStatus();
                     }
-                });
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Login Fail:" + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
